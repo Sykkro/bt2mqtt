cbe94e0eece113bbba888042a823b344957bb022
Ricardo Sousa <2652879+Sykkro@users.noreply.github.com>
2020-08-12 10:26:20 +0100

    support config dir and hci device override

--- bt2mqtt.sh
+++ bt2mqtt.sh
@@ -13,35 +13,36 @@
 #
 # This script does the following steps:
 #
-# 1 - read devices.conf for device=MAC mappings and assume devices presence is -1
+# 1 - read $BT2MQTT_FILE_NAME for device=MAC mappings and assume devices presence is -1
 # 2 - store default bluetooth page timeout and set a custom one
 # 3 - start while true loop, set cycle to 0 and repeat after POLL seconds
-# 4 - increase cycle by 1 % CHECK_PRESENCE_CYCLE
-# 5 - every 1/CHECK_PRESENCE_CYCLE cycles, if there are 100% present devices, check if they are still present
-# 6 - every cycle, if there are devices with presence below 100%, update their presence (either reduce by DROP_RATE if absent or set it to 100 if present)
+# 4 - increase cycle by 1 % BT2MQTT_CHECK_PRESENCE_CYCLE
+# 5 - every 1/BT2MQTT_CHECK_PRESENCE_CYCLE cycles, if there are 100% present devices, check if they are still present
+# 6 - every cycle, if there are devices with presence below 100%, update their presence (either reduce by BT2MQTT_DROP_RATE if absent or set it to 100 if present)
 # 7 - wait POLL seconds and repeat from 4
 # 8 - on exit, restore the default timeout
 
 
 #-------- STATIC CONFIGS --------#
-: ${POLL_INTERVAL:=3}           # main while loop interval in seconds
-: ${CHECK_PRESENCE_CYCLE:=10}   # every 1/CHECK_PRESENCE_CYCLE cycles, check present
-: ${DROP_RATE:=25}              # 1-100 rate at which a device becomes missing
-: ${HCI_TIMEOUT_SECONDS:=2}     # timeout value to be set when attempting to ping a bluetooth device
-: ${HCI_INTERFACE:="hci0"}      # HCI interface name
-: ${L2PING_TIMEOUT:=1}          # timeout between l2ping checks
-: ${EXEC_TIMEOUT:=3}            # maximum time to wait for l2ping execution results
-: ${MQTT_HOST:="localhost"}     # MQTT host address
-: ${MQTT_PORT:="1883"}          # MQTT host port
-: ${MQTT_CLIENT_ID:="BT2MQTT"}  # MQTT publisher client ID
-: ${MQTT_TOPIC:="bt2mqtt"}     # MQTT base topic path (should start with, but not end with, a slash)
-: ${MQTT_QOS:=1}                # MQTT QOS setting (0 for at most once, 1 for at least once, 2 for exactly once)
+: ${BT2MQTT_FILE_NAME:="conf/devices.conf"}  # file path for the configured bluetooth devices to scan
+: ${BT2MQTT_POLL_INTERVAL:=3}                # main while loop interval in seconds
+: ${BT2MQTT_CHECK_PRESENCE_CYCLE:=10}        # every 1/BT2MQTT_CHECK_PRESENCE_CYCLE cycles, check present
+: ${BT2MQTT_DROP_RATE:=25}                   # 1-100 rate at which a device becomes missing
+: ${BT2MQTT_HCI_TIMEOUT_SECONDS:=2}          # timeout value to be set when attempting to ping a bluetooth device
+: ${BT2MQTT_HCI_INTERFACE:="hci0"}           # HCI interface name
+: ${BT2MQTT_L2PING_TIMEOUT:=1}               # timeout between l2ping checks
+: ${BT2MQTT_EXEC_TIMEOUT:=3}                 # maximum time to wait for l2ping execution results
+: ${BT2MQTT_MQTT_HOST:="localhost"}          # MQTT host address
+: ${BT2MQTT_MQTT_PORT:="1883"}               # MQTT host port
+: ${BT2MQTT_MQTT_CLIENT_ID:="BT2MQTT"}       # MQTT publisher client ID
+: ${BT2MQTT_MQTT_TOPIC:="bt2mqtt"}           # MQTT base topic path (should start with, but not end with, a slash)
+: ${BT2MQTT_MQTT_QOS:=1}                     # MQTT QOS setting (0 for at most once, 1 for at least once, 2 for exactly once)
 
 #TODO: add MQTT auth support
 
 # no need to change down from here
 HCI_ONE_SECOND_BLOCKS=1600 # used to calculate HCI timeout blocks
-(( HCI_TIMEOUT_BLOCKS = HCI_TIMEOUT_SECONDS * HCI_ONE_SECOND_BLOCKS ))
+(( HCI_TIMEOUT_BLOCKS = BT2MQTT_HCI_TIMEOUT_SECONDS * HCI_ONE_SECOND_BLOCKS ))
 MQTT_TEMPLATE='{"presence":%s, "mac":"%s", "uptime":%s}'
 LOOP=true
 
@@ -55,13 +56,12 @@ function log() {
 }
 #### BLUETOOTH MANAGEMENT ####
 
-# TODO: assuming hci0 here
 function getDefaultTimeout() {
-	hciconfig hci0 pageto | sed -n -E "s/.*Page timeout: (.*) slots.*/\\1/p"
+	hciconfig $BT2MQTT_HCI_INTERFACE pageto | sed -n -E "s/.*Page timeout: (.*) slots.*/\\1/p"
 }
-# TODO: assuming hci0 here
+
 function setTimeout() {
-	hciconfig $HCI_INTERFACE pageto $1
+	hciconfig $BT2MQTT_HCI_INTERFACE pageto $1
 }
 
 #### MQTT MESSAGING ####
@@ -72,13 +72,13 @@ function publishEvent() {
     uptime=$4
     #"$(date +%s%N)"
     message=$(printf "$MQTT_TEMPLATE" "$presence" "$addr" "$uptime")
-    mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -i $MQTT_CLIENT_ID -q $MQTT_QOS \
-                  -t "$MQTT_TOPIC/$device" -m "$message"
+    mosquitto_pub -h $BT2MQTT_MQTT_HOST -p $BT2MQTT_MQTT_PORT -i $BT2MQTT_MQTT_CLIENT_ID -q $BT2MQTT_MQTT_QOS \
+                  -t "$BT2MQTT_MQTT_TOPIC/$device" -m "$message"
 }
 
 #### DEVICE MANAGEMENT ####
 function checkDevice() {
-	timeout -t $EXEC_TIMEOUT l2ping -s 0 -d 0 -c 1 -t $L2PING_TIMEOUT $1
+	timeout -t $BT2MQTT_EXEC_TIMEOUT l2ping -s 0 -d 0 -c 1 -t $BT2MQTT_L2PING_TIMEOUT $1
 }
 
 function updatePresence() {
@@ -95,7 +95,7 @@ function updatePresence() {
         newPresence=100
     else
         # device is down, decrement presence
-        newPresence=$(( newPresence > DROP_RATE ? (newPresence - DROP_RATE) : 0 ))
+        newPresence=$(( newPresence > BT2MQTT_DROP_RATE ? (newPresence - BT2MQTT_DROP_RATE) : 0 ))
     fi
 
 
@@ -140,16 +140,22 @@ function checkNonPresent() {
 
 #--------------------------------#
 
-# TODO: ensure we have access to an active bluetooth device
 # ensure we're running as root
 if [ "$EUID" -ne 0 ]; then
     log "Please run as root"
     exit 1
 fi
 
+# ensure required binaries are installed
+ls &> /dev/null || { echo "ls not found" ; exit 1; }
+hcitool &> /dev/null || { echo "hcitool not found" ; exit 1; }
+hciconfig &> /dev/null || { echo "hciconfig not found" ; exit 1; }
+
+# ensure configured device exists
+(hcitool dev | grep "$BT2MQTT_HCI_INTERFACE") &> /dev/null || { echo "Interface '$BT2MQTT_HCI_INTERFACE' not found" ; exit 1; }
+
 # 1 - read devices from devices.conf file and assume devices presence is -1
-fileName="conf/devices.conf"
-readarray -t lines < "$fileName"
+readarray -t lines < "$BT2MQTT_FILE_NAME"
 declare -A devices
 declare -A presences
 declare -A discoveredAt
@@ -177,19 +183,19 @@ function onQuit() {
 cycle=0
 while $LOOP
 do
-    # 4 - increase cycle by 1 % CHECK_PRESENCE_CYCLE
-    (( cycle=(cycle+1) % CHECK_PRESENCE_CYCLE ))
+    # 4 - increase cycle by 1 % BT2MQTT_CHECK_PRESENCE_CYCLE
+    (( cycle=(cycle+1) % BT2MQTT_CHECK_PRESENCE_CYCLE ))
 
-    # 5 - every 1/CHECK_PRESENCE_CYCLE cycles, if there are present devices, check if they are still present and reduce presence by DROP_RATE if they aren't
-    if ! ((cycle % CHECK_PRESENCE_CYCLE)); then
+    # 5 - every 1/BT2MQTT_CHECK_PRESENCE_CYCLE cycles, if there are present devices, check if they are still present and reduce presence by BT2MQTT_DROP_RATE if they aren't
+    if ! ((cycle % BT2MQTT_CHECK_PRESENCE_CYCLE)); then
         confirmPresent devices presences discoveredAt
     fi
 
     # 6 - every cycle, if there are non-present devices, check if they become present and change presence to 100 if they are
     checkNonPresent devices presences discoveredAt
 
-    # 7 - wait POLL_INTERVAL seconds and repeat from 4
-    sleep $POLL_INTERVAL
+    # 7 - wait BT2MQTT_POLL_INTERVAL seconds and repeat from 4
+    sleep $BT2MQTT_POLL_INTERVAL
 done
 
 # 8 - on exit, restore the default timeout
